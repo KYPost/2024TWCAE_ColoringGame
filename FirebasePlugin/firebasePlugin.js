4b96a0b60cced28f3067f5d60671aceb1387109b
KYPost <kypostdesign@gmail.com>
2024-10-17 10:12:42 +0800

    initial commit

---
+++ FirebasePlugin/firebasePlugin.js
@@ -0,0 +1,14 @@
+// Firebase的金鑰
+const firebaseConfig = {
+  apiKey: "AIzaSyDjxSi3Dqb8NZkIkHLyO_AQsQu4B1HlZFk",
+  authDomain: "twcae-moea-coloringgame.firebaseapp.com",
+  databaseURL: "https://twcae-moea-coloringgame-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "twcae-moea-coloringgame",
+  storageBucket: "twcae-moea-coloringgame.appspot.com",
+  messagingSenderId: "657001711866",
+  appId: "1:657001711866:web:2d76548c534444ef62e143",
+  measurementId: "G-M9CQV8FZCT"
+};
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
